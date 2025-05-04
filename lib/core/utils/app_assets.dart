adf388b51691a115e91cd782188789c5d40fc0a1
khaled hussien <20013@zood.ai>
2025-05-05 00:18:33 +0300

    app assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesDalel
+  /// assets/images/Dalel.png
+  static const String imagesDalel = "assets/images/Dalel.png";
+
+  /// Assets for imagesForgetPass
+  /// assets/images/forgetPass.png
+  static const String imagesForgetPass = "assets/images/forgetPass.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesSucccesicon
+  /// assets/images/Succcesicon.png
+  static const String imagesSucccesicon = "assets/images/Succcesicon.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/vector1.svg
+  static const String imagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/vector2.svg
+  static const String imagesVector2 = "assets/images/vector2.svg";
+}
